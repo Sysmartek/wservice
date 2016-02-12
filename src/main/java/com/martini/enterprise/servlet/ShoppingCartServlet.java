c2274ac9b51154f1c2a2a67335b5703904ecf0b6
omartini38 <osvaldo.martini@gmail.com>
2016-02-12 10:19:36 -0300

    imports

--- src/main/java/com/martini/enterprise/servlet/ShoppingCartServlet.java
+++ src/main/java/com/martini/enterprise/servlet/ShoppingCartServlet.java
@@ -1,20 +1,15 @@
 package com.martini.enterprise.servlet;
 
 import java.io.IOException;
-import java.util.Hashtable;
 
 import javax.annotation.ManagedBean;
 import javax.ejb.EJB;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.martini.enterprise.ejb.CartBeanImpl;
 import com.martini.enterprise.ejb.CartRemote;
 import com.tutorialspoint.model.Product;
 

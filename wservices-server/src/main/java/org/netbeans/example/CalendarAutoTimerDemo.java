96c886f694dc0d688f0263648ba0be6021e05d14
omartini38 <osvaldo.martini@gmail.com>
2016-02-16 10:39:52 -0300

    NOVOS TIMERS SCHEDULING

---
+++ wservices-server/src/main/java/org/netbeans/example/CalendarAutoTimerDemo.java
@@ -0,0 +1,23 @@
+package org.netbeans.example;
+
+import java.util.Date;
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+import javax.ejb.Schedule;
+import javax.ejb.Startup;
+import javax.ejb.Timer;
+
+@Singleton
+@LocalBean
+public class CalendarAutoTimerDemo {
+
+    @Schedule(second="13,34,57", minute="*", hour="*")
+    public String execute(Timer timer) {
+        System.out.println("Executing ...");
+        System.out.println("Execution Time : " + new Date());
+        System.out.println("____________________________________________");  
+        
+        return "Execution Time : " + new Date();
+        
+    }
+}

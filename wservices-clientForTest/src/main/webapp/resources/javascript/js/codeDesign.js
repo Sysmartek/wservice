ec1e96d460efe7fafccaf75b544afe4521a6f040
omartini38 <osvaldo.martini@gmail.com>
2016-02-23 07:02:18 -0300

    comit geral

--- wservices-clientForTest/src/main/webapp/resources/javascript/js/codeDesign.js
+++
@@ -1,9 +0,0 @@
-
-	//resize text area
-	function resizeTextArea(elem) {
-		elem.height(1);
-		elem.scrollTop(0);
-		elem
-				.height(elem[0].scrollHeight - elem[0].clientHeight
-						+ elem.height());
-	}
